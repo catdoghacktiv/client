3cd0ac4f727bd50c013ad0337ee88dc5e96bf8ef
hafrizresa <hafrizresa@gmail.com>
2018-12-14 10:31:19 +0700

    rebased from dev

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,5 @@
 import firebase from 'firebase'
 
-<<<<<<< HEAD
 export default 
 firebase.initializeApp({
   apiKey: "AIzaSyBefuslLHGwu-jh97D2HgwSwz8ha-Zae4o",
@@ -11,13 +10,3 @@ firebase.initializeApp({
   messagingSenderId: "1065403431088"
 });
 
-=======
-export default firebase.initializeApp({
-    apiKey: "AIzaSyBnUmTwAm1Kt2RMNPGlwx94hKC3T0l8puA",
-    authDomain: "blogv2-ef0c4.firebaseapp.com",
-    databaseURL: "https://blogv2-ef0c4.firebaseio.com",
-    projectId: "blogv2-ef0c4",
-    storageBucket: "blogv2-ef0c4.appspot.com",
-    messagingSenderId: "732398837464"
-  })
->>>>>>> add loby and room feature
