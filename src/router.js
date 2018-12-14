8be5d131d487fc4d1234eb9eebe065d27d07093c
hafrizresa <hafrizresa@gmail.com>
2018-12-14 10:37:30 +0700

    fix conflict

--- src/router.js
+++ src/router.js
@@ -13,11 +13,11 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
     {
       path: '/hit',
       name: 'hit',
