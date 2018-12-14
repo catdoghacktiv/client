782080828a074b0a578ac54527d05c8844fcc127
hafrizresa <hafrizresa@gmail.com>
2018-12-14 10:37:30 +0700

    fix conflict

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> | 
-      <router-link to="/hit">hit</router-link>
-    </div>
-    <router-view/>
-  </div>
-=======
   <router-view/>
->>>>>>> add loby and room feature
 </template>
 
 <style>
